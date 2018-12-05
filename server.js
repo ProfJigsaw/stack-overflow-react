5c8fecb6dd7e87a00eb626c8d1e41446c2d85b9d
ProfJigsaw <dhagodfather@gmail.com>
2018-12-05 19:36:31 +0100

    ch(deployment): deploy base branch to heroku
    - install express
    - setup webserver

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.static('dist'));
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+    console.log('Server started on port: ', PORT);
+});
