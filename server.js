209e458c9f4f3474343ed2fcb81ac872da7a4bb0
ProfJigsaw <dhagodfather@gmail.com>
2018-12-17 13:24:30 +0100

    Incorporate secure font awesome cdn

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 
@@ -6,6 +7,14 @@ app.use(express.static('dist'));
 
 const PORT = process.env.PORT || 5001;
 
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'), (err) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
 app.listen(PORT, () => {
-    console.log('Server started on port: ', PORT);
+  console.log('Server started on port: ', PORT);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app_bundle.js',
+    publicPath: '/'
   },
   module: {
     rules: [
